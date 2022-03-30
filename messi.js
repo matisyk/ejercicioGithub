e2db776c0259faef22e700755a91dd76a6642b93
matisyk <matisyk@hotmail.com>
2022-03-29 21:11:39 -0300

    todo junto

--- messi.js
+++ messi.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 const pipa = "por abajo";
 let fuego = function(e){
     return e + 1;
 }
 let goles = 1;
-=======
 let messi = campeon;
 console.log("mundial Qatar")
 let prueba = "que pasaria si se cruzan las lineas "
 console.log ("alfajores de dulce de leche")
->>>>>>> 17d60c8d1202ca0d900ba61fb7415020189a6c2d
